8c93f5c51291ac364b3a032b1aad0f18c23c9570
ls30378 <lorentsh@gmail.com>
2023-11-14 23:24:24 +0100

    auth guards, strategies, login

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -6,6 +6,8 @@ import { LoggerModule } from 'nestjs-pino';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
+import { LocalStrategy } from './strategies/local.strategy';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [
@@ -31,6 +33,6 @@ import * as Joi from 'joi';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,16 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly userRepository: UsersRepository) {}
-
-  async create(createUserDto: CreateUserDTO) {
+  async validateUser(email: string, password: string) {
     try {
-      return createUserDto;
+      const user = await this.userRepository.findOne({ email });
+      const passwordIsValid = await bcrypt.compare(password, user.password);
+      if (!passwordIsValid) {
+        throw new UnauthorizedException();
+      }
+      return user;
     } catch (error) {
       throw error;
     }
   }
+
+  async create(createUserDto: CreateUserDTO) {
+    try {
+      return this.userRepository.create({
+        ...createUserDto,
+        password: await bcrypt.hash(createUserDto.password, 10),
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+  constructor(private readonly userRepository: UsersRepository) {}
 }
